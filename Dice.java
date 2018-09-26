b75d1780fe3170f93647986ee95453f32c3a3670
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2018-09-26 18:16:07 -0500

    +

--- Dice.java
+++ Dice.java
@@ -1,18 +1,16 @@
 import java.util.Random;
 
 public class Dice {
-   /*
-   *
-   */
+   
    private int sidesOfDice;
   
    //the value of the side currently facing up
    private int valueOfSideFacingUp;
   
-   //to randomize the sides
+   
    Random dice;
   
-   // this Constructor wont take any argument
+   
    public Dice(){
        sidesOfDice=6;
        dice = new Random();
@@ -22,7 +20,7 @@ public class Dice {
       
    }
   
-   //this constructor accepts single argument
+   
    public Dice(int x){
       
        if(x==4 || x==6 || x==8 || x==10 || x==12 || x==20 || x==100)
